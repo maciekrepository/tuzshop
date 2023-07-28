c9216bb4d984a3ce8db9ed797475584bf18ecf86
macieklazarz <macieklazarz@gmail.com>
2023-07-28 22:37:31 +0200

    20230728_2

--- tuzshop/settings.py
+++ tuzshop/settings.py
@@ -141,3 +141,19 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
+EMAIL_HOST = 'plskm33.hostsila.org'.strip('\u200e')
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'admin@tuzownia.pl'.strip('\u200e')
+EMAIL_HOST_PASSWORD = 'C1eramik@'.strip('\u200e')
+EMAIL_USE_TLS = True
